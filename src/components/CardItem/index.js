c76c61f3fed1358843fda67df9a839f6265bb7af
Sateesh <sateeshkosuri1997@gmail.com>
2022-08-19 09:40:02 +0000

    Technology Cards tests

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+
+const Cards = props => {
+  const {cardDetails, key} = props
+  const {title, description, imgUrl, className} = cardDetails
+  return (
+    <li>
+      <div className="card-container">
+        <div className={`${className}`}>
+          <h1 className="card-title">{title}</h1>
+          <p className="card-description">{description}</p>
+          <div className="image-container">
+            <img src={imgUrl} className="card-image" alt={title} />
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default Cards
