177d09fafdc7b94b399c00e353863b66d6f9d04c
sateeshkosuri <109948165+sateeshkosuri@users.noreply.github.com>
2022-08-23 18:30:10 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -6,7 +6,7 @@ const Cards = props => {
   return (
     <li>
       <div className="card-container">
-        <div className={`${className}`}>
+        <div className={className}>
           <h1 className="card-title">{title}</h1>
           <p className="card-description">{description}</p>
           <div className="image-container">
